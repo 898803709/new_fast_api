a4b8edb5e66904bf987a81a5d227b52fbd0325ed
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-07-14 19:35:50 +0900

    Add reloading to Uvicorn command, refactor get_secret_key function

--- api/get_secret_key.py
+++ api/get_secret_key.py
@@ -3,21 +3,23 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def get_secret(secret_name: str, key_name: str) -> str:
-    secret_name = secret_name
-    region_name = "ap-northeast-1"
+class AWSClient:
+    @classmethod
+    def get_secret(cls, secret_name: str, key_name: str) -> str:
+        secret_name = secret_name
+        region_name = "ap-northeast-1"
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
+        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        client = session.client(service_name="secretsmanager", region_name=region_name)
 
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-    except ClientError as e:
-        raise e
+        try:
+            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        except ClientError as e:
+            raise e
 
-    secret = json.loads(get_secret_value_response["SecretString"])
-    return secret[key_name]
+        secret = json.loads(get_secret_value_response["SecretString"])
+        return secret[key_name]
 
 
-print(get_secret("openai", "OpenAI"))
+print(AWSClient.get_secret("openai", "OpenAI"))
