30cfc6ac71f8fb055bb986b60f8c1f7e40ba68b6
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-07-22 23:19:00 +0900

    Add CRUD operations for tasks and done items

--- api/db.py
+++ api/db.py
@@ -1,14 +1,14 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
-async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
+async_session = sessionmaker(bind=async_engine, class_=AsyncSession, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 
 
-def get_db():
-    with async_session() as session:
+async def get_db():
+    async with async_session() as session:
         yield session
